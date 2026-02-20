d95766069159ddc101581d0acc37d49fc63b1459
SakithNizar <sakithmohamed54@gmail.com>
2026-02-20 13:49:42 +0530

    chore: create Django project and providers app

--- providers/models.py
+++ providers/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Provider(models.Model):
+    business_name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=20)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.business_name
