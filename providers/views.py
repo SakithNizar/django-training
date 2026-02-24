29b0309f3cfeea6321694e90ba83cc3c3a866dc7
SakithNizar <sakithmohamed54@gmail.com>
2026-02-24 12:30:23 +0530

    feat(providers): add ordering and multi-field search using Q objects

--- providers/views.py
+++ providers/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from django.db.models import Q
+from .models import Provider
 
-# Create your views here.
+
+class ProviderListView(ListView):
+    model = Provider
+    template_name = "providers/provider_list.html"
+    context_object_name = "providers"
+    ordering = ["business_name"]
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        search = self.request.GET.get("search")
+
+        if search:
+            queryset = queryset.filter(
+                Q(business_name__icontains=search) |
+                Q(email__icontains=search)
+            )
+
+        return queryset
