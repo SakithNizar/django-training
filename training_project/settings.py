5302490d98173a04e5847c669294093c5d6cfdba
SakithNizar <sakithmohamed54@gmail.com>
2026-02-27 11:48:37 +0530

    feat(auth): configure default authentication classes in DRF

--- training_project/settings.py
+++ training_project/settings.py
@@ -125,3 +125,13 @@ REST_FRAMEWORK = {
         "rest_framework.filters.OrderingFilter",
     ],
 }
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.TokenAuthentication",
+    ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.AllowAny",
+    ],
+}
