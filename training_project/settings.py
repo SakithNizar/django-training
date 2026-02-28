e5ac266607e9e960e2f4bafef1fc7d56352e481c
SakithNizar <sakithmohamed54@gmail.com>
2026-02-28 12:00:33 +0530

    feat(security): configure JWT authentication and Django security settings

--- training_project/settings.py
+++ training_project/settings.py
@@ -129,8 +129,6 @@ REST_FRAMEWORK = {
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
-        "rest_framework.authentication.SessionAuthentication",
-        "rest_framework.authentication.TokenAuthentication",
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ],
     "DEFAULT_PERMISSION_CLASSES": [
